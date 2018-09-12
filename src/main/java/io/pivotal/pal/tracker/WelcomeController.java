0c72a25344623513d4b9a2624dae78de4204af04
Mark Lugo <lugo.mark@gmail.com>
2018-09-12 10:33:26 -0400

    updated get method name

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/")
-    public String sayHell() {
+    public String sayHello() {
         return "hello";
     }
 
